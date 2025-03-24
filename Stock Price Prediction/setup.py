28dbfbf2b071e6ded026d7b1e9b94f124acee52e
Tanishq Saxena <tanishq.saxena2@gmail.com>
2025-03-24 11:17:57 +0530

    This is my Stock Price Prediction web app

---
+++ Stock Price Prediction/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='your_package_name',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'tensorflow',
+    ],
+)
